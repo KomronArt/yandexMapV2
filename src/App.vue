e2aa2856559501afe9e3dec8445e47e20a51310e
KomronArt <65872176+KomronArt@users.noreply.github.com>
2023-01-22 12:27:09 +0500

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-	<button
+  <button
     v-for="item in items"
     :key="item.id"
     type="button"
     @click="setMapCenter(item)"
   >
-    изменить позицию {{item.title}}
+    изменить позицию {{ item.title }}
   </button>
-	<yandex-map
+  <yandex-map
     :coordinates="coordinates"
     :detailed-controls="detailedControls"
     :controls="controls"
@@ -20,58 +20,55 @@
       :coordinates="[item.lat, item.lon]"
     />
   </yandex-map>
-  </template>
-  
-  <script setup>
-  import { ref } from "vue"
-  import { YandexMap, YandexMarker } from 'vue-yandex-maps';
+</template>
 
-  const items = [
-        {
-          id: 999,
-          title: "Европочта, Отделение № 999",
-          subtitle: "г.Минск, ул.Ангарская, 62А",
-          lat: "53.87999999999999",
-          lon: "27.683455306746517",
-          rating: "4,6",
-        },
-        {
-          id: 123,
-          title: "Европочта, Отделение № 123",
-          subtitle: "Минск, ул. Сурганова, 54",
-          lat: "53.87222222222222",
-          lon: "27.683455306746517",
-          rating: "4,6",
-        },
-        {
-          id: 156,
-          title: "Европочта, Отделение № 6",
-          subtitle: "Минск, ул. Притыцкого, 156",
-          lat: "53.87660859946719",
-          lon: "27.683455306746517",
-          rating: "4,6",
-        },
-      ]
-	  let mapObject = '';
-      const coordinates = [53.87660859946719, 27.683455306746517];
-      const controls = ["fullscreenControl"];
-      const detailedControls = { zoomControl: { position: { right: 10, top: 50 } } }
+<script setup>
+import { ref } from "vue";
+import { YandexMap, YandexMarker } from "vue-yandex-maps";
 
-	const onMapCreated = (map) => {
-		mapObject = map;
-	};
-	
-    const setMapCenter = (item) => {
-		const coords = [item.lat, item.lon]
-		mapObject.setCenter(coords, 17);
-	};
-	
-  
-  </script>
-  
-  <style>
-  .yandex-container {
-	height: 400px;
-  }
-  
-  </style>
+const items = [
+  {
+    id: 999,
+    title: "Европочта, Отделение № 999",
+    subtitle: "г.Минск, ул.Ангарская, 62А",
+    lat: "53.87999999999999",
+    lon: "27.683455306746517",
+    rating: "4,6",
+  },
+  {
+    id: 123,
+    title: "Европочта, Отделение № 123",
+    subtitle: "Минск, ул. Сурганова, 54",
+    lat: "53.87222222222222",
+    lon: "27.683455306746517",
+    rating: "4,6",
+  },
+  {
+    id: 156,
+    title: "Европочта, Отделение № 6",
+    subtitle: "Минск, ул. Притыцкого, 156",
+    lat: "53.87660859946719",
+    lon: "27.683455306746517",
+    rating: "4,6",
+  },
+];
+let mapObject = "";
+const coordinates = [53.87660859946719, 27.683455306746517];
+const controls = ["fullscreenControl"];
+const detailedControls = { zoomControl: { position: { right: 10, top: 50 } } };
+
+const onMapCreated = (map) => {
+  mapObject = map;
+};
+
+const setMapCenter = (item) => {
+  const coords = [item.lat, item.lon];
+  mapObject.setCenter(coords, 17);
+};
+</script>
+
+<style>
+.yandex-container {
+  height: 400px;
+}
+</style>
